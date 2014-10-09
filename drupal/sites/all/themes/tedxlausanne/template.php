e19a3b3aa18b146f379592292f696f8d2a0bb526
Marc Friederich <marc@antistatique.net>
2014-10-09 19:49:51 +0200

    Add specific display type for content type template

--- drupal/sites/all/themes/tedxlausanne/template.php
+++ drupal/sites/all/themes/tedxlausanne/template.php
@@ -12,3 +12,17 @@ function tedxlausanne_preprocess_page(&$variables) {
 
 
 }
+
+/**
+ * Preprocess variables for node.tpl.php
+ *
+ * @see node.tpl.php
+ */
+function tedxlausanne_preprocess_node(&$variables) {
+
+  // That will let you use a template file like: node--[type|nodeid]--teaser.tpl.php
+  if($variables['view_mode'] == 'teaser') {
+    $variables['theme_hook_suggestions'][] = 'node__' . $variables['node']->type . '__teaser';
+  }
+
+}
