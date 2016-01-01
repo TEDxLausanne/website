57c6b7586a4ec5f270e9e96692f7171bb59a2c69
Marc Friederich <marc@antistatique.net>
2016-01-01 19:19:23 +0100

    Add facebook tracking code`

--- drupal/sites/all/themes/tedxlausanne/templates/system/html.tpl.php
+++ drupal/sites/all/themes/tedxlausanne/templates/system/html.tpl.php
@@ -83,6 +83,22 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/flexie/1.0.3/flexie.js"></script>
   <![endif]-->
   <?php print $scripts; ?>
+  <!-- Facebook Conversion Code for Paiements - TED X LAUSANNE -->
+  <script>(function() {
+    var _fbq = window._fbq || (window._fbq = []);
+    if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+    }
+    })();
+    window._fbq = window._fbq || [];
+    window._fbq.push(['track', '6037733502525', {'value':'0.00','currency':'CHF'}]);
+  </script>
+  <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6037733502525&amp;cd[value]=0.00&amp;cd[currency]=CHF&amp;noscript=1" /></noscript>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
 <!-- Google Tag Manager -->
